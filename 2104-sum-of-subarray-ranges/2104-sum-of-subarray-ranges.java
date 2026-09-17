6339509549e2584165bbb86f03ebcbc105c481a1
samm-4 <samanthagrace01234@gmail.com>
2026-09-17 20:52:22 +0530

    Time: 28 ms (39.01%), Space: 47 MB (20.11%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,115 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+
+        long small = min(nums);
+        long big = max(nums);
+        return big-small;
+    }
+    static long min(int [] arr)
+    {
+        int n = arr.length;
+        int [] l = new int[n];
+        int [] r = new int[n];
+        Stack<Integer> st = new Stack<>();
+        //pse
+        for(int i=0; i<n; i++)
+        {
+            while(!st.isEmpty() && arr[i] < arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                l[i] = i+1;
+            }
+            else
+            {
+                l[i] = i-st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+
+        //nse
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!st.isEmpty() && arr[i] <= arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                r[i] = n-i;
+            }
+            else
+            {
+                r[i] = st.peek()-i;
+            }
+            st.push(i);
+        }
+
+        //int mod = 1_000_000_007;
+        long sum =0;
+        for(int i=0; i<n; i++)
+        {
+            long curr = (long) l[i]*r[i]*arr[i];
+            sum += curr;
+        }
+        return sum;
+
+    }
+    static long max(int [] arr)
+    {
+        int n = arr.length;
+        int [] l = new int[n];
+        int [] r = new int[n];
+        Stack<Integer> st = new Stack<>();
+        //pge
+        for(int i=0; i<n; i++)
+        {
+            while(!st.isEmpty() && arr[i] > arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                l[i] = i+1;
+            }
+            else
+            {
+                l[i] = i-st.peek();
+            }
+            st.push(i);
+        }
+        st.clear();
+
+        //nge
+        for(int i=n-1; i>=0; i--)
+        {
+            while(!st.isEmpty() && arr[i] >= arr[st.peek()])
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                r[i] = n-i;
+            }
+            else
+            {
+                r[i] = st.peek()-i;
+            }
+            st.push(i);
+        }
+
+        //int mod = 1_000_000_007;
+        long sum =0;
+
+        for(int i=0; i<n; i++)
+        {
+            long curr = (long) l[i]*r[i]*arr[i];
+            sum += curr;
+        }
+        return sum;
+
+    }
+}
